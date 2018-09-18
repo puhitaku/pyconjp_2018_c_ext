1d0de319ae97c306c8d6aad99038ff17b4ff44ca
Takumi Sueda <puhitaku@gmail.com>
2018-09-18 14:30:29 +0900

    Implement Py3 module init

--- src/helloworld.c
+++ src/helloworld.c
@@ -23,7 +23,22 @@ static PyMethodDef methods[] = {
     {NULL},
 };
 
+#if PY_MAJOR_VERSION >= 3
+static struct PyModuleDef def = {
+    PyModuleDef_HEAD_INIT,
+    "libhelloworld",
+    NULL,
+    -1,
+    methods
+};
+
+PyMODINIT_FUNC PyInit_libhelloworld(void) {
+    PyObject *module = PyModule_Create(&def);
+    return module;
+}
+#else
 void initlibhelloworld(void) {
     Py_InitModule("libhelloworld", methods);
 }
+#endif
 
