9d2aaab3f7a7864441bb2eb04700a7a3e3a78599
Takumi Sueda <puhitaku@gmail.com>
2018-09-18 14:29:28 +0900

    Fix __init__.py

--- helloworld/__init__.py
+++ helloworld/__init__.py
@@ -1,10 +1,11 @@
+from __future__ import print_function
 import libhelloworld
 
 
 def hello():
-    print libhelloworld.hello()
+    print(libhelloworld.hello())
 
 
 def hello_hex():
-    print repr(libhelloworld.hello_hex())
+    print(repr(libhelloworld.hello_hex()))
 
